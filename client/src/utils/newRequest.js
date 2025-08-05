5dfc2b51f98072f5156ea80893a8f6aa85e76a98
TenzinTsogney <tsogney99@gmail.com>
2025-08-05 11:54:44 +0530

    Initial fullstack commit

--- client/src/utils/newRequest.js
+++ client/src/utils/newRequest.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: "http://localhost:8800/api/",
-  withCredentials: true,
+  baseURL: "baseURL: import.meta.env.VITE_API_URL" || "http://localhost:8800/api",
+  withCredentials: true, // needed for cookies
 });
 
 export default newRequest;
